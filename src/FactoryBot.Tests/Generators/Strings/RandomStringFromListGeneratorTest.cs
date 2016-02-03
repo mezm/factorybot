7db3ea918281893eea247bb08005faf1a8091e9c
Valeriy <bartleby.sev@gmail.com>
2016-02-03 16:50:02 +0200

    added random string from list generator

---
+++ src/FactoryBot.Tests/Generators/Strings/RandomStringFromListGeneratorTest.cs
@@ -0,0 +1,24 @@
+﻿using FactoryBot.Generators.Strings;
+using NUnit.Framework;
+
+namespace FactoryBot.Tests.Generators.Strings
+{
+    [TestFixture]
+    public class RandomStringFromListGeneratorTest
+    {
+        [Test]
+        public void Generate()
+        {
+            var list = new[] {"a", "ab", "bc", "def"};
+            var generator = new RandomStringFromListGenerator(list);
+            var str = (string) generator.Next();
+            Assert.That(list, Does.Contain(str));
+        }
+
+        [Test]
+        public void CreateWithEmptyList()
+        {
+            Assert.That(() => new RandomStringFromListGenerator(new string[0]), Throws.ArgumentException);
+        }
+    }
+}
---
+++ src/FactoryBot/Generators/Strings/RandomStringFromListGenerator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FactoryBot.Generators.Strings
+{
+    public class RandomStringFromListGenerator : TypedGenerator<string>
+    {
+        private readonly IReadOnlyList<string> _source;
+
+        public RandomStringFromListGenerator(IReadOnlyList<string> source)
+        {
+            Check.NotNull(source, nameof(source));
+            if (source.Count == 0)
+            {
+                throw new ArgumentException("Source should not be empty.", nameof(source));
+            }
+
+            _source = source;
+        }
+
+        protected override string NextInternal()
+        {
+            var index = NextRandomInteger(0, _source.Count - 1);
+            return _source[index];
+        }
+    }
+}
