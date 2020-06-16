3f7fb9646ab5d17c865f5278203b2ef1cd2757f9
mezm <bartleby.sev@gmail.com>
2020-06-16 12:09:40 +0300

    [wip] samples

---
+++ src/FactoryBot.Samples/AllTypesModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace FactoryBot.Samples
+{
+    public class AllTypesModel
+    {
+        public int Integer { get; set; }
+
+        public long Long { get; set; }
+
+        public short Short { get; set; }
+
+        public byte Byte { get; set; }
+
+        public double Double { get; set; }
+
+        public float Float { get; set; }
+
+        public decimal Decimal { get; set; }
+
+        public bool Boolean { get; set; }
+
+        public string String { get; set; }
+
+        public DateTime DateTime { get; set; }
+
+        [JsonConverter(typeof(TimeSpanConverter))]
+        public TimeSpan TimeSpan { get; set; }
+
+        public override string ToString() => JsonSerializer.Serialize(this);
+    }
+}
---
+++ src/FactoryBot.Samples/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace FactoryBot.Samples
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var count = 5;
+            if (args.Length != 0 && int.TryParse(args[0], out var argCount))
+            {
+                count = argCount;
+            }
+
+            AutoDefine(count);
+        }
+
+        private static void AutoDefine(int count)
+        {
+            Bot.DefineAuto<AllTypesModel>();
+            var model = Bot.Build<AllTypesModel>();
+            Console.WriteLine("AutoDefine:");
+            Bot.BuildSequence<AllTypesModel>().Take(count).ToList().ForEach(x => Console.WriteLine(x));
+        }
+    }
+}
