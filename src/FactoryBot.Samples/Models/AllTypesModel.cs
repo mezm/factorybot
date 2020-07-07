c2773d6e4a1689dbae4e9605cf51a4343d16c1db
Valeriy <bartleby.sev@gmail.com>
2020-07-07 13:27:08 +0300

    Add Samples Project (#30)
    * [wip] samples
    
    * added enum
    
    * fixed enum serialization
    
    * added manual sample

---
+++ src/FactoryBot.Samples/Models/AllTypesModel.cs
@@ -0,0 +1,38 @@
+﻿using FactoryBot.Samples.Converters;
+using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace FactoryBot.Samples.Models
+{
+    public class AllTypesModel
+    {
+        public int Integer { get; set; }
+
+        public long Long { get; set; }
+
+        public short Short { get; set; }
+
+        public byte Byte { get; set; }
+
+        public double Double { get; set; }
+
+        public float Float { get; set; }
+
+        public decimal Decimal { get; set; }
+
+        public bool Boolean { get; set; }
+
+        public string String { get; set; }
+
+        public DateTime DateTime { get; set; }
+
+        [JsonConverter(typeof(TimeSpanConverter))]
+        public TimeSpan TimeSpan { get; set; }
+
+        [JsonConverter(typeof(EnumConverter))]
+        public EnumModel Enum { get; set; }
+
+        public override string ToString() => JsonSerializer.Serialize(this, new JsonSerializerOptions { WriteIndented = true });
+    }
+}
---
+++ src/FactoryBot.Samples/Program.cs
@@ -0,0 +1,55 @@
+﻿using FactoryBot.Samples.Models;
+using System;
+using System.Linq;
+
+namespace FactoryBot.Samples
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var count = 5;
+            if (args.Length != 0 && int.TryParse(args[0], out var argCount))
+            {
+                count = argCount;
+            }
+
+            AutoDefine(count);
+
+            Console.WriteLine();
+            Bot.ForgetAll();
+            ManualDefine(count);
+        }
+
+        private static void AutoDefine(int count)
+        {
+            Bot.DefineAuto<AllTypesModel>();
+            var model = Bot.Build<AllTypesModel>();
+            Console.WriteLine("Auto Define:");
+            Bot.BuildSequence<AllTypesModel>().Take(count).ToList().ForEach(x => Console.WriteLine(x));
+        }
+
+        private static void ManualDefine(int count)
+        {
+            Bot.Define(x => new AllTypesModel
+            {
+                Boolean = x.Boolean.Any(),
+                Byte = x.Byte.RandomFromList(3, 4, 5, 10, 12, 100),
+                DateTime = x.Dates.AfterNow(),
+                Decimal = x.Decimal.Any(-1000000, 1000000),
+                Double = x.Double.SequenceFromList(12.45, 66.1234, 444.4),
+                Enum = x.Enums.RandomFromList(EnumModel.First, EnumModel.Last),
+                Float = x.Float.Any(),
+                Integer = x.Integer.Any(0, 500),
+                Long = x.Long.Any(),
+                Short = x.Short.Any(-100, 100),
+                String = x.Address.Country(),
+                TimeSpan = x.Time.Any()
+            });
+
+            var model = Bot.Build<AllTypesModel>();
+            Console.WriteLine("Manual Define:");
+            Bot.BuildSequence<AllTypesModel>().Take(count).ToList().ForEach(x => Console.WriteLine(x));
+        }
+    }
+}
