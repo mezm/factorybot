03b22c9c4dd41fcd8bf655022e2b5e608ee5d025
mezm <bartleby.sev@gmail.com>
2020-07-07 13:08:03 +0300

    added manual sample

--- src/FactoryBot.Samples/Program.cs
+++ src/FactoryBot.Samples/Program.cs
@@ -15,13 +15,40 @@ namespace FactoryBot.Samples
             }
 
             AutoDefine(count);
+
+            Console.WriteLine();
+            Bot.ForgetAll();
+            ManualDefine(count);
         }
 
         private static void AutoDefine(int count)
         {
             Bot.DefineAuto<AllTypesModel>();
             var model = Bot.Build<AllTypesModel>();
-            Console.WriteLine("AutoDefine:");
+            Console.WriteLine("Auto Define:");
+            Bot.BuildSequence<AllTypesModel>().Take(count).ToList().ForEach(x => Console.WriteLine(x));
+        }
+
+        private static void ManualDefine(int count)
+        {
+            Bot.Define(x => new AllTypesModel
+            {
+                Boolean = x.Boolean.Any(),
+                Byte = x.Byte.RandomFromList(3, 4, 5, 10, 12, 100),
+                DateTime = x.Dates.AfterNow(),
+                Decimal = x.Decimal.Any(-1000000, 1000000),
+                Double = x.Double.SequenceFromList(12.45, 66.1234, 444.4),
+                Enum = x.Enums.RandomFromList(EnumModel.First, EnumModel.Last),
+                Float = x.Float.Any(),
+                Integer = x.Integer.Any(0, 500),
+                Long = x.Long.Any(),
+                Short = x.Short.Any(-100, 100),
+                String = x.Address.Country(),
+                TimeSpan = x.Time.Any()
+            });
+
+            var model = Bot.Build<AllTypesModel>();
+            Console.WriteLine("Manual Define:");
             Bot.BuildSequence<AllTypesModel>().Take(count).ToList().ForEach(x => Console.WriteLine(x));
         }
     }
